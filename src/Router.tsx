13ffea3af4b0e568b2292115a1dc3faab3b4a6fa
Gabriel Teles <gabrielfariateles@gmail.com>
2023-11-30 16:39:49 -0300

    ✨ feat: created Router element and Home page.

---
+++ src/Router.tsx
@@ -0,0 +1,11 @@
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
