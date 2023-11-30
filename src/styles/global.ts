543c00ab01473ac8d1a058ff1cef75793bc68027
Gabriel Teles <gabrielfariateles@gmail.com>
2023-11-30 16:27:28 -0300

    ✨ feat: created global styles, defaultTheme, ThemeProvider using defaultTheme

---
+++ src/styles/global.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+    font-size: 62.5%;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  body, input, button, textarea {
+    font-family: 'Nunito', sans-serif;
+    font-size: 1.6rem;
+    font-weight: 400;
+  }
+`;
